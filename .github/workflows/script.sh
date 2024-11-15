8154650bd8fd40f2a65686ff8562d9c646829872
Fredrik Hammar <fhammar@volvocars.com>
2024-11-15 13:25:42 +0100

    another commit

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -1,12 +1,1 @@
-  if [[ "" == "" ]]; then
-    IMAGE_TAG="empty"
-    echo "OUTPUT=$IMAGE_TAG" >> $GITHUB_ENV
-  elif [[ "main.75b2aa34" != "" ]]; then
-    IMAGE_TAG="payload"
-    echo "OUTPUT=$IMAGE_TAG" >> $GITHUB_ENV
-  else
-    BRANCH=${GITHUB_REF_NAME}
-    VERSION=${GITHUB_SHA::8}
-    IMAGE_TAG="${BRANCH}.${VERSION}"
-    echo "OUTPUT=$IMAGE_TAG" >> $GITHUB_ENV
-  fi
+COMMIT=$(echo "main.1234" | cut -d '.' -f 1)
