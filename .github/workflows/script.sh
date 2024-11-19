9fdde3f01c243ce73e228f138b3013d655ebc801
Fredrik Hammar <fhammar@volvocars.com>
2024-11-19 10:23:37 +0100

    does this work?

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -1,1 +1,8 @@
-COMMIT=$(echo "main.1234" | cut -d '.' -f 1)
+
+myArray=("cat" "dog" "mouse" "frog")
+
+
+# Check if an element is in the array and length is greater than 1
+if [[ ${myArray[@]} =~ "cat" && ${#myArray[@]} -gt 1 ]]; then
+  echo "cat is in the array"
+fi
